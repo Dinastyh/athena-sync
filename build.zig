b1fd1741d09e26e5900bf553ffb0f519e3e582c8
Paul LEGE <paul.lege@protonmail.com>
2024-07-17 11:22:50 +0800

    chore(build): add target and optimize args for athena-sync module

--- build.zig
+++ build.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule(
         "athena-sync",
         .{
             .root_source_file = b.path("src/athena-sync.zig"),
+            .target = target,
+            .optimize = optimize,
         },
     );
 }
